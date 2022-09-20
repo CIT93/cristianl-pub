265310fca8f50f3c4a471e63fccb371194d8b509
Cristian Lemus <cristianislemus@gmail.com>
2022-09-20 08:28:33 +0000

    Put comments in W4 Assignment

--- w4/app.js
+++ w4/app.js
@@ -6,6 +6,9 @@ const showOnPage = function (text) {
   };
 //ShowOnPage Function
 
+
+// I made an object contained the name and size of my starbucks order and also a method that will return me my order based on what time i woke up and if I have money in my account.
+// The current time variable: For simplicity I am assuming it is AM. Any PM values would have to be used in 24 hr time.
 let starbucksOrder = { 
     name: 'Caramel Frappe',
     size: 'Small',
